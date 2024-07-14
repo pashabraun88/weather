d9e0304ef0096a21909afff48396fe7c3458e321
Pavlo Zelenka <pzelenka30898@gmail.com>
2024-07-14 15:35:18 +0300

    moved index.html

--- src/pages/Home/components/ThisDay/ThisDay.tsx
+++ src/pages/Home/components/ThisDay/ThisDay.tsx
@@ -1,42 +1,23 @@
 
 import { GlobalSvgSelector } from '../../../../assets/icons/global/GlobalSvgSelector';
 import s from './ThisDay.module.scss';
-import { useWeather } from '../../../../context/WeatherContext';
-import { getIconId } from '../../../../utils/iconMapper'
 
 
 interface Props{}
 
-export const ThisDay: React.FC = () => {
-    
-    const {data} = useWeather();
-
-    const getLocalTime  = (timeZoneOffset: number) => {
-        const utcTime = new Date().getTime()
-        const localTime = new Date (utcTime + timeZoneOffset * 1000);
-        return new Intl.DateTimeFormat('uk-Ua', 
-        {
-            hour: '2-digit',
-            minute: '2-digit',
-            timeZone: 'UTC',
-        }).format(localTime);
-    };
-
-
+export const ThisDay = (props: Props) => {
     return (
-        
     <div className={s.this__day}>
         <div className={s.top__block}>
             <div className={s.top__block_wrapper}>
-                <div className={s.this__temp}>{data ?(<span className={s.this__temp_number}>{Math.floor(data.main.temp)}°C</span>) : null}</div>
+                <div className={s.this__temp}>25°</div>
                 <div className={s.this__day_name}>Сьогодні</div>
             </div>
-            {data && data.weather[0] && (<GlobalSvgSelector id={getIconId(data.weather[0].icon)} /> )}
-        
+        <GlobalSvgSelector id="sun"/>
         </div>
         <div className={s.bottom__block}>
-            <div className={s.this__time}>Година: <span>{data ? getLocalTime(data.timezone) : null}</span></div>
-            <div className={s.this__city}>Місто: {data ? (<span>{data.name}</span>) : null}</div>
+            <div className={s.this__time}>Година: <span>04:20</span></div>
+            <div className={s.this__city}>Місто: <span>Львів</span></div>
         </div>
     </div>
     )
